cd5a42a42e2a6ef16b5b9237f82ca139a7c7101b
Mak <infinitycorpkz@gmail.com>
2022-10-17 10:36:13 +0600

    cards refactored and get request data

--- script.js
+++ script.js
@@ -151,70 +151,28 @@ window.addEventListener('DOMContentLoaded', () => {
          this.parent.append(element);
       }
    }
-   new Card(
-      'Bounty Hunter',
-      'img/Trading_Card_Icon_-_Bounty_Hunter.webp',
-      'bh',
-      `Bounty Hunter — активный герой поддержки, предпочитающий сложную линию.
-   Ориентирован на убийство одиноких целей. Благодаря Jinada Jinada, Bounty Hunter наносит много урона с
-   атаки и ворует золото у вражеских героев. В начале игры это позволяет эффективно размениваться
-   ресурсами с противником и мешать ему фармить. Shadow Walk Shadow Walk дарует Bounty Hunter невидимость
-   и замедление со следующей атаки. Под действием Shadow Walk Shadow Walk Bounty Hunter может разведывать
-   территорию для поиска легкой цели. Не лишним будет взять с собой Sentry Ward Sentry Ward и Observer
-   Ward Observer Ward, чтобы проверить наличие вражеских вардов и расставить свои. Shuriken Toss Shuriken
-   Toss наносит урон, накладывает микро-оглушение и отскакивает к помеченным Track Track противникам. Это
-   позволяет добить уходящего врага или прервать прочтение заклинания. Aghanim's Scepter Aghanim's
-   Scepter увеличивает дальность применения Shuriken Toss Shuriken Toss и добавляет ей эффект Jinada
-   Jinada. Track Track раскрывает невидимость и показывает цель в тумане войны. Позволяет Bounty Hunter
-   передвигаться быстрее и наносить по ней критический урон. За убийство противника под Track Track
-   Bounty Hunter и его команда получают бонусное золото, что помогает быстро набрать преимущество над
-   противником при активной игре. Track Track также показывает количество золота у вражеского героя. В
-   начале игры Bounty Hunter легко перемещается по линиям и помогает команде совершать убийства. В
-   дальнейшем — выполняет роль разведчика, показывая местоположение противника. Основная цель Bounty
-   Hunter в драке — вражеские саппорты, так как ему проще до них добираться под Shadow Walk Shadow Walk.`,
-      579,
-      '.cards__body'
-   ).render();
 
-   new Card(
-      'Tusk',
-      'img/tusk.webp',
-      'tusk',
-      'Снежным комом врывается во врагов, оглушая их Tusk никогда не против затеять драку. Он отрезает врагам путь к отступлению стеной ледяных осколков, собирает союзников в гигантский снежный ком и влетает в нём в противников, завершая комбинацию фирменным сногсшибательным ударом.',
-      900,
-      '.cards__body'
-   ).render();
+   const getResource = async (url) => {
+      const res = await fetch(url)
 
-   new Card(
-      'Vengeful Spirit',
-      'img/Dota_2_Card_8.webp',
-      'venge',
-      `Vengeful Spirit — это герой с дальним типом атаки, основным атрибутом которого
-   является Agility attribute symbol.png ловкость. Ее первая способность, Magic Missile, выпускает во
-   врага магический снаряд, который оглушает и наносит урон. Второй способностью, Wave of Terror,
-   Vengeful Spirit издает дьявольский клич, который ослабляет броню врагов и раскрывает туман войны.
-   Пассивная способность, Vengeance Aura, увеличивает урон ближайших союзников. Так же при смерти героя
-   на ее месте появится иллюзия Vengerful Spirit наносящая 100% урона героя и 150% получаемого,после
-   возврождения героя — дух исчезнет. Ультимативная способность Nether Swap, позволяет меняеться местами
-   с союзным или вражеским героем, так же способность сбивает анимации способностей вражеский персонажей.
-   Улучшение с Aghanim's Scepter icon.png Aghanim's Scepter применяет страх на врагов вокруг места
-   появления Vengerful Spirit после обмена местами с выбранным существоооом.`,
-      819,
-      '.cards__body'
-   ).render();
+      if (!res.ok) {
+         throw new Error(`Could not fetch ${url}, status:${res.status}`)
+      }
 
-   new Card(
-      'Riki',
-      'img/Dota_2_Card_4.webp',
-      'riki',
-      'Riki - это герой с ближним типом атаки, основным атрибутом которого является Agility attribute symbol.png ловкость. Его первая способность, Smoke Screen, бросает дымовую бомбу, замедляя скорость передвижения вражеских существ в зоне охвата, не позволяя им колдовать и заставляя их промахиваться в большинстве случаев. Вторая способность, Blink Strike, Телепортируется за спину выбранного юнита и наносит дополнительный урон, если это вражеский юнит. Третья способность, Tricks of the Trade, Riki пропадает из нашего мира, атакуя со спины случайных противников в радиусе вокруг себя. Ультимативная пассивная способность, Cloak and Dagger, Riki скрывается в тени, становясь невидимым. Если Riki атакует со спины, то он наносит дополнительный урон, зависящий от его текущей ловкости.',
-      717,
-      '.cards__body'
-   ).render();
+      return await res.json()
+   }
+
+   getResource('http://localhost:3000/menu')
+      .then(data => {
+         data.forEach(({ title, img, altimg, descr, price }) => {
+            new Card(title, img, altimg, descr, price, '.cards__body').render();
+         })
+      });
+
+
 
 
    ///////////Отправка данных на сервер///////
-
    const forms = document.querySelectorAll('form');
 
    const message = {
@@ -224,10 +182,21 @@ window.addEventListener('DOMContentLoaded', () => {
    }
 
    forms.forEach(item => {
-      postData(item);
+      bindPostData(item);
    })
 
-   function postData(form) {
+   const postData = async (url, data) => {
+      const res = await fetch(url, {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: data
+      });
+      return await res.json()
+   }
+
+   function bindPostData(form) {
       form.addEventListener('submit', (e) => {
          e.preventDefault();
 
@@ -240,19 +209,9 @@ window.addEventListener('DOMContentLoaded', () => {
          form.insertAdjacentElement('afterend', statusMessage);
 
          const formData = new FormData(form);
+         const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-         const object = {};
-         formData.forEach(function (value, key) {
-            object[key] = value;
-         });
-
-         fetch('server.php', {
-            method: 'POST',
-            headers: {
-               'Content-type': 'application/json'
-            },
-            body: JSON.stringify(object)
-         }).then(data => data.text())
+         postData('http://localhost:3000/requests', json)
             .then(data => {
                console.log(data);
                showThanksModal(message.success);
